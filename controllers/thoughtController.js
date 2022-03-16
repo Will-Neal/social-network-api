c38519353be7002ba495c5070ef0aa41c29b4ca7
Will Neal <willwneal@gmail.com>
2022-03-16 16:15:57 -0400

    PUT route and controller to update thought by ID

--- controllers/thoughtController.js
+++ controllers/thoughtController.js
@@ -17,6 +17,14 @@ module.exports = {
         const newThought = await Thought.create(req.body);
         res.json(`New Thought created successfully...`)
     },
+    //PUT Update thought by id
+    async updateThought(req, res) {
+        const updatedThought = await Thought.findOneAndUpdate(
+            {_id:req.params.thoughtId},
+            {$set: req.body},
+            {new:true});
+        res.json(`Thought id ${updatedThought._id} updated successfully...`)
+    },
     // DELETE thought by id
     async deleteThought(req, res) {
         const deletedThought = await Thought.findOneAndDelete({ _id:req.params.thoughtId});
--- routes/api/thoughtRoutes.js
+++ routes/api/thoughtRoutes.js
@@ -3,13 +3,14 @@ const {
     getThoughts,
     getSingleThought,
     postNewThought,
-    deleteThought
+    deleteThought,
+    updateThought
 } = require('../../controllers/thoughtController.js')
 
 //Route for all thoughts
 router.route('/').get(getThoughts).post(postNewThought);
 
 //Route for single thought
-router.route('/:thoughtId').get(getSingleThought).delete(deleteThought);
+router.route('/:thoughtId').get(getSingleThought).put(updateThought).delete(deleteThought);
 
 module.exports = router;
