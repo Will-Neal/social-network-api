03dcf98bad17d26ad5d9b59d9acca2fc8afea6b7
Will Neal <willwneal@gmail.com>
2022-03-16 09:22:40 -0400

    Added create and delete user routes and tested

--- controllers/userController.js
+++ controllers/userController.js
@@ -1,13 +1,25 @@
+const res = require('express/lib/response')
 const { User } = require('../models')
 
 module.exports = {
+    //Get All Users
     async getUsers(req, res) {
         const users = await User.find({})
         res.send(users)
     },
-
+    //Get Single User by ID
     async getSingleUser(req, res) {
-        const singleUser = await User.findOne({ _id: req.params.userId})
+        const singleUser = await User.findOne({ _id: req.params.userId});
         res.json(singleUser)
+    },
+    //Post a new user
+    async postNewUser(req, res) {
+        const newUser = await User.create(req.body);
+        res.json(newUser)
+    },
+    //Delete a User
+    async deleteUser(req, res) {
+        const deletedUser = await User.findOneAndDelete({ _id:req.params.userId});
+        res.json(deletedUser)
     }
 }
--- routes/api/userRoutes.js
+++ routes/api/userRoutes.js
@@ -1,13 +1,15 @@
 const router = require('express').Router();
 const {
     getUsers,
-    getSingleUser
+    getSingleUser,
+    postNewUser,
+    deleteUser
 } = require('../../controllers/userController.js')
 
 //Route for all thoughts
-router.route('/').get(getUsers);
+router.route('/').get(getUsers).post(postNewUser);
 
 //Route for single thought
-router.route('/:userId').get(getSingleUser);
+router.route('/:userId').get(getSingleUser).delete(deleteUser);
 
 module.exports = router;
